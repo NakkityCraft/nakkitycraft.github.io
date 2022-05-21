87c005fce0e4cd5d0f3c27f1f7f67d9928061e2e
Chenken520 <57039359+Choneas@users.noreply.github.com>
2022-05-21 14:03:12 +0800

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -2,13 +2,13 @@
 /* Get Out Of There.
    Your mom.         */
 /* My Script */
-var $ = mdui.$;
-$('#beta').on('click', function () {
+var mdui = mdui.$;
+mdui('#beta').on('click', function () {
   mdui.snackbar({
     message: 'Sorry, This page is locked, you need a cookie.',
     position: 'bottom'
   });
 });
-$("#jk").click(function(){
+mdui("#jk").click(function(){
   $('body').toggleClass('mdui-theme-layout-dark')
 });
